a03c4195c5898d8f223daaeafb2b5d95f2ea2a75
Data Center SIS-INF <datacenter@sistemas.edu.bo>
2023-09-27 14:44:09 -0400

    First commit

---
+++ src/components/AddCategory.jsx
@@ -0,0 +1,30 @@
+import { useState } from 'react'
+
+export const AddCategory = ({ onNewCategory }) => {
+
+    const [inputValue, setInputValue] = useState('')
+
+    const onInputChange = (e) => {
+        setInputValue(e.target.value)
+    }
+
+    const onSubmit = (e) => {
+        e.preventDefault()
+        const newInputValue = inputValue.trim()
+        if (newInputValue.length <= 1) return;
+        // setCategories(categories => [inputValue, ...categories])
+        onNewCategory(newInputValue)
+        setInputValue('')
+    }
+
+    return (
+
+        <form onSubmit={onSubmit}>
+            <input
+                type='text'
+                placeholder='Buscar Gif'
+                value={inputValue}
+                onChange={onInputChange} />
+        </form>
+    )
+}
